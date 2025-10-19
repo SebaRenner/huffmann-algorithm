6eac244d375b2a3aa17d1ad835322863b02c8599
SebaRenner <sebastian.renner@gmx.ch>
2025-10-19 15:04:06 +0200

    Added FileProcessor and HuffmanFile husk

---
+++ Huffman.IO/FileProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace Huffman.IO;
+
+public static class FileProcessor
+{
+    public static byte[] ReadBytesFromFile(string filePath)
+    {
+        return File.ReadAllBytes(filePath);
+    }
+}
---
+++ Huffman.IO/HuffmanFile.cs
@@ -0,0 +1,7 @@
+﻿namespace Huffman.IO;
+
+// To be implemented
+public class HuffmanFile
+{
+    // Propeties like the original file name, file extension, encoded bytes, huffman tree or table
+}
